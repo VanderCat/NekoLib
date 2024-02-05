0e3b17258003b416627448dd1f074abef31dadf5
Влад Кот <VanderOfCat@gmail.com>
2024-02-05 23:37:12 +0300

    [DOC] Document Component API

--- NekoLib/Core/Behaviour.cs
+++ NekoLib/Core/Behaviour.cs
@@ -1,8 +1,17 @@
 ﻿namespace NekoLib.Core; 
 
+/// <summary>
+/// A component that can be turned off
+/// </summary>
 public abstract class Behaviour : Component {
+    /// <summary>
+    /// Is this component enabled or not
+    /// </summary>
     public bool Enabled = true;
     
+    /// <summary>
+    /// Is this component enabled AND parent GameObject is active too. If it is false the behaviour will not update.
+    /// </summary>
     public bool IsActiveAndEnabled => GameObject.Active && Enabled;
     
 }
--- NekoLib/Core/Component.cs
+++ NekoLib/Core/Component.cs
@@ -2,15 +2,35 @@
 
 namespace NekoLib.Core; 
 
+/// <summary>
+/// Main building block of the logic inside GameObjects
+/// </summary>
 public abstract class Component : Object {
+    
+    /// <summary>
+    /// GameObject this component part of
+    /// </summary>
     public GameObject GameObject { get; internal set; }
 
+    /// <summary>
+    /// Tag of the component
+    /// </summary>
     public string Tag = "";
 
+    /// <summary>
+    /// Alias for this.GameObject.Transform
+    /// </summary>
     public Transform Transform => GameObject.Transform;
 
+    /// <summary>
+    /// Alias for <c>this.GameObject.Broadcast(string)</c>
+    /// </summary>
     public void Broadcast(string methodName) => GameObject.Broadcast(methodName);
     
+    /// <summary>
+    /// Find and run Method inside this Component
+    /// </summary>
+    /// <param name="methodName">Name of the method to run</param>
     public void Invoke(string methodName) {
         var method = GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
         
