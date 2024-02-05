e0769b6b3d2bc64a906b30dbbf076f1220ae8de0
Влад Кот <VanderOfCat@gmail.com>
2024-02-05 23:22:20 +0300

    [DOC] Document Object.cs

--- NekoLib/Core/Object.cs
+++ NekoLib/Core/Object.cs
@@ -1,7 +1,18 @@
 ﻿namespace NekoLib.Core; 
 
+/// <summary>
+/// Base object to derive from, provides name and Id for stuff
+/// </summary>
 public abstract class Object : IDisposable{
+    /// <summary>
+    /// Name of the object
+    /// </summary>
+    /// <remarks>this should be visible in hierarchy</remarks>
     public string Name = "";
+    
+    /// <summary>
+    /// A randomly generated id of the object
+    /// </summary>
     public Guid Id = Guid.NewGuid();
 
     public new string ToString() => Name;
