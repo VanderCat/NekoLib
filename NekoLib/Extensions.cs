e4b8bc7b7a5021d72883b28ed623ad0d358f62cd
Влад Кот <vanderofcat@gmail.com>
2024-06-14 16:31:59 +0300

    [API] Extensions for querying a scene - Added IScene.GetRootGameObjects() - Added IScene.GetGameObjectByName(string name) - Added IScene.GetGameObjectById(Guid id) - Added IScene.GetComponentById(Guid id) - Added GameObject.GetChildByName(string name)

--- NekoLib/Extensions.cs
+++ NekoLib/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Numerics;
+using NekoLib.Core;
+using NekoLib.Scenes;
 
 namespace NekoLib; 
 
@@ -12,5 +14,29 @@ public static class Extensions {
         angles.Z = MathF.Atan2(2.0f * (r.X * r.Y + r.Z * r.W), 1.0f - 2.0f * (r.X * r.X + r.Z * r.Z));
         
         return angles;
+    }
+    
+    public static List<GameObject> GetRootGameObjects(this IScene scene) {
+        return scene.GameObjects.Where(o => o.Transform.Parent is null).ToList();
+    }
+
+    public static GameObject? GetGameObjectByName(this IScene scene, string name) {
+        return scene.GameObjects.First(o => o.Name == name);
+    }
+    public static GameObject? GetGameObjectById(this IScene scene, Guid id) {
+        return scene.GameObjects.FirstOrDefault(o => o.Id == id);
+    }
+    
+    public static Component? GetComponentById(this IScene scene, Guid id) {
+        foreach (var gameObject in scene.GameObjects) {
+            var component = gameObject.GetComponentById(id);
+            if (component is not null)
+                return component;
+        }
+        return null;
+    }
+
+    public static GameObject? GetChildByName(this GameObject gameObject, string name) {
+        return gameObject.Transform.First(o => o.GameObject.Name == name).GameObject;
     }
 }
